e9cd9e1f6e9836e7e667bf809519faa0f1471140
kenta0905 <77146336+kenta0905@users.noreply.github.com>
2021-01-29 23:01:17 +0900

    2 コメント投稿機能 hoge

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,7 +1,9 @@
 class Prototype < ApplicationRecord
 
-  belongs_to :user
+  belongs_to :user, dependent: :destroy
   has_one_attached :image
+  has_many :comments, dependent: :destroy
+
 
   validates :title, presence: true
   validates :catch_copy, presence: true
