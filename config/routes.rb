4b678a94359aee9526fb6fe9de9ae274df4ab0e6
kenta0905 <77146336+kenta0905@users.noreply.github.com>
2021-02-01 12:35:14 +0900

    5 適切なビューファイル

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   root to: 'prototypes#index'
   resources :prototypes, only: [:create, :new, :index, :show, :edit, :update, :destroy] do
   resources :comments, only: [:create]
+  resources :users, only: :show
   end
 end
